03b1ac2d6c2e27eb7706e1fee27303e967621af6
KFCSpike <KFCSpike@brillyunt.com>
2013-02-27 14:02:56 +0000

    Fix for Add Artefacts from admin menu

--- GameEngine/Admin/Mods/addArtefacts.php
+++ GameEngine/Admin/Mods/addArtefacts.php
@@ -12,6 +12,7 @@
 ini_set('max_execution_time', 1000);
 error_reporting(E_ALL);
 
+include_once("../../Database/connection.php");
 include_once("../../config.php");
 include_once("../../Database/db_MYSQL.php");
 
