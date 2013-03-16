e7aff68c1d690cb8e97bce93f92038fc90df6444
MiguelSam <ptbr@live.com.pt>
2013-03-16 14:05:22 +0000

    Translation

--- Admin/ManageNews.php
+++ Admin/ManageNews.php
@@ -4,17 +4,17 @@
 	</ul>
 </div><Br>			
 <div id="div_1">
-<div id="succes" class="MsgHead success" style="display:none;"><div class="MsgBody">عمليات با موفقيت انجام شد.</div></div><br>
+<div id="succes" class="MsgHead success" style="display:none;"><div class="MsgBody">Operation was successful.</div></div><br>
 <table width="350" border="1" align="center" class="tbl" id="tabs_2_content_in">
     <tbody>
         <tr>
-            <td bgcolor="#d1dde0" height="26" align="center"><b>عنوان</b></td>
-            <td bgcolor="#d1dde0" height="26" align="center"><b>وضعیت</b></td>
-            <td bgcolor="#d1dde0" height="26" align="center"><b>نمایش</b></td>
-            <td align="center" bgcolor="#d1dde0"><b>عملیات</b></td>
+            <td bgcolor="#d1dde0" height="26" align="center"><b>Address</b></td>
+            <td bgcolor="#d1dde0" height="26" align="center"><b>Status</b></td>
+            <td bgcolor="#d1dde0" height="26" align="center"><b>Show</b></td>
+            <td align="center" bgcolor="#d1dde0"><b>Operations</b></td>
         </tr>
 	<tr>
-        <td bgcolor="#f3f7f8" height="26" align="center">خبر شماره 1</td>
+        <td bgcolor="#f3f7f8" height="26" align="center">News No. 1</td>
         <td bgcolor="#f3f7f8" height="26" align="center"><?php if(NEWSBOX1){ echo "<div id='nsts1'><font color='green'>فعال</font></div>"; }else{ echo "<div id='nsts1'><font color='red'>غیر فعال</font></div>"; } ?></td>
         <td bgcolor="#f3f7f8" height="26" align="center">
         بلی &nbsp; خیر<p>
@@ -25,7 +25,7 @@
         	<a href="?p=News&n=1"><img border="0" src="images/edit.gif" /></a></td>
 	</tr>
     <tr>
-        <td bgcolor="#f3f7f8" height="26" align="center">خبر شماره 2</td>
+        <td bgcolor="#f3f7f8" height="26" align="center">News No. 2</td>
         <td bgcolor="#f3f7f8" height="26" align="center"><?php if(NEWSBOX2){ echo "<div id='nsts2'><font color='green'>فعال</font></div>"; }else{ echo "<div id='nsts2'><font color='red'>غیر فعال</font></div>"; } ?></td>
         <td height="26" align="center" bgcolor="#f3f7f8"> بلی &nbsp; خیر
           <p>
@@ -36,7 +36,7 @@
         	<a href="?p=News&n=2"><img border="0" src="images/edit.gif" /></a></td>
 	</tr>
     <tr>
-    	<td bgcolor="#f3f7f8" height="26" align="center">خبر شماره 3</td>
+    	<td bgcolor="#f3f7f8" height="26" align="center">News No. 3</td>
         <td bgcolor="#f3f7f8" height="26" align="center"><?php if(NEWSBOX3){ echo "<div id='nsts3'><font color='green'>فعال</font></div>"; }else{ echo "<div id='nsts3'><font color='red'>غیر فعال</font></div>"; } ?></td>
         <td height="26" align="center" bgcolor="#f3f7f8"> بلی &nbsp; خیر
           <p>
